2cf3968e67e7c3ecf536d05b6d8ec827cfdbf2bd
edza <edexdev@gmail.com>
2016-08-02 18:15:04 +0300

    Initial commit

---
+++ EdzaMarkup/MainWindow.xaml.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace EdzaMarkup
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        string Parse(string text)
+        {
+            StringBuilder output = new StringBuilder();
+            var lines = text.Split(new[] { '\n', '\r' }, StringSplitOptions.RemoveEmptyEntries);
+
+            foreach(var line in lines)
+            {
+                if (line.StartsWith("-"))
+                    output.AppendLine("<ol><li>" + line + "</li></ol>");
+                else
+                    output.AppendLine(line);
+            }
+
+            return output.ToString();
+        }
+    }
+}
